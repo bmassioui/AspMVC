d13a815c4ed699e9209682f1eaba63934f788a1e
bouchaib-massioui <bouchaib.massioui@gmail.com>
2021-11-06 11:33:25 +0100

    Create Models

---
+++ src/WebApp/Models/TvShow.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Models
+{
+    public class TvShow
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public decimal Rating { get; set; }
+
+        public string ImdbUrl { get; set; }
+    }
+}
